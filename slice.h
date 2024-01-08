e19bb42c321d7525961af7f2e61b23f969f4801d
Devin Frost <devin.frost4311@gmail.com>
2024-01-07 21:48:08 -0500

    Added documentation

--- slice.h
+++ slice.h
@@ -5,21 +5,37 @@ class str_slice
 {
 public:
 
+	// Create a string slice.
 	str_slice(char* start, size_t length)
 	{
 		mStart = start;
 		mLength = length;
 	}
 
+	// Get the length of the string view
 	inline const size_t len() const
 	{
 		return mLength;
 	}
+
+	// Get a pointer to the start of the string view.
 	inline const char* start() const
 	{
 		return mStart;
 	}
 
+	// Get a copy of the string view. If you want to treat the string view as a char*, use this.
+	inline const char* get() const
+	{
+		const char* buffer = (const char*)malloc(mLength);
+		if (buffer)
+		{
+			memcpy(buffer, mStart, mLength);
+		}
+		return buffer;
+	}
+
+	// Printing a string view
 	friend std::ostream& operator << (std::ostream& os, const str_slice& slice)
 	{
 		os.write(slice.start(), slice.len());
